262860bba392768d5e3ed1152bebe6c6c568a0ae
Pedro Navarrete Hurtado <navhpedro@gmail.com>
2018-03-12 18:43:44 -0500

    Finished Lab 10, exercise 3

--- comp151Lab10/grades.cpp
+++ comp151Lab10/grades.cpp
@@ -2,9 +2,10 @@
 #include <iostream>
 using namespace std;
 
-// PLACE YOUR NAME HERE
+// Pedro J. Navarrete
+// V1.00
 
-const MAXNAME = 20;
+const int MAXNAME = 20;
 
 int main()
 {
@@ -21,10 +22,13 @@ int main()
 		inData >> average;
 		// FILL IN THE CODE to print out name and 
 		// student average
+		cout << name << "has a(n) " << average << " average" << endl;
 
 		// FILL IN THE CODE to complete the while 
 		// loop so that the rest of the student
 		// names and average are read in properly
+		inData.ignore(1);
+		inData.get(name,MAXNAME+1);
 	}
 
 	return 0;
